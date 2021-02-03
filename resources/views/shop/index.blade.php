aa35e46fa1d5fd22bf0529118ff9ace1a2929dbb
Your Name <you@example.com>
2021-02-03 16:47:11 +0100

    add routes

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -31,13 +31,13 @@
     </div>
 </nav>
 
-<div class="position-relative overflow-hidden bg-Gvs p-3 p-md-5 m-md-3 text-center bg-light"  style="background-image: url({{ asset('Banner2.png') }})">
+<!--<div class="position-relative overflow-hidden bg-Gvs p-3 p-md-5 m-md-3 text-center bg-light"  style="background-image: url({{ asset('Banner2.png') }})">
     <div class="col-md-5 p-lg-5 mx-auto margClass ">
         <img src="{{ asset('Schriftzug-weiss.png') }}">
         <p class="lead font-weight-normal" style="font-size: 25px; color: dimgrey"><b>Die beste Wahl für ihren Einkauf. Gourmet Wallis hat die besten Regional Produkte. Unterstüzen sie die Region.</b></p>
-        <a href="{{route('shop_index')}}" class="btn btn-lg btn-dark">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jetzt einkaufen&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+        <a href="{{route('shop_index')}}" class="btn btn-dark">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jetzt einkaufen&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
     </div>
-</div>
+</div>-->
 
 
 <footer class="container py-5">
