1b20b08fc8dc5f03d1eb21d6e6fe98dec9ad4c94
stefan <dani.stevenson@web.de>
2021-02-10 15:38:29 +0100

    add Factory

---
+++ database/factories/DeclarationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Declaration;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DeclarationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Declaration::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
