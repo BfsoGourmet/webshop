fc829b4228246877579292a7e6001e2f95975a6f
Your Name <you@example.com>
2021-02-24 17:00:37 +0100

    Add Custom Contoller

--- app/Http/Controllers/Api/V1/ApiOrderController.php
+++ app/Http/Controllers/Api/V1/ApiOrderController.php
@@ -12,15 +12,17 @@ class ApiOrderController extends Controller
     {
         $last_order_id = $request->get('last_order_id');
         if ($last_order_id) {
-            return order::where('id', '>', $last_order_id)->with('productOrders', 'customer', 'status', 'shippingAddress', 'billingAddress')->get();
+            $orders =  order::where('id', '>', $last_order_id)->with('productOrders', 'customer', 'status', 'shippingAddress', 'billingAddress')->get();
         }
         else {
-            return order::with('productOrders', 'customer', 'status', 'shippingAddress', 'billingAddress')->get();
+            $orders = order::with('productOrders', 'customer', 'status', 'shippingAddress', 'billingAddress')->get();
         }
+        return $this->jsonResponse($orders);
     }
 
-    public function show(order $order)
+    public function show(int $order_id)
     {
-        return $order;
+        $order = order::where('id', '=', $order_id)->with('productOrders', 'customer', 'status', 'shippingAddress', 'billingAddress')->get();
+        return $this->jsonResponse($order);
     }
 }
