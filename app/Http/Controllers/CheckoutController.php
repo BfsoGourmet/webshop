12445fbd57b79fdc6a9660e365a44fc4203f0581
unknown <you@example.com>
2021-03-10 16:56:58 +0100

    Orderprocess added

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CheckoutRequest;
+use App\Models\Address;
+use App\Models\Customer;
+use App\Models\Order;
+use App\Models\Product;
 
 class CheckoutController extends Controller
 {
@@ -11,7 +15,52 @@ class CheckoutController extends Controller
     }
 
     public function store(CheckoutRequest $request) {
-/*        return redirect(route('create'))->withSuccess(__('form.successfully-stored'));*/
+        $order = new order();
+        $deliveryAddress = new address();
+        $deliveryAddress->address = $request->addressDelivery;
+        $deliveryAddress->zipcode = $request->postCodeDelivery;
+        $deliveryAddress->city = $request->cityDelivery;
+
+        $customerDelivery = new customer();
+        $customerDelivery->firstname = $request->firstNameDelivery;
+        $customerDelivery->lastname = $request->lastNameDelivery;
+        $customerDelivery->phone = $request->phoneDelivery;
+        $customerDelivery->email = $request->emailDelivery;
+
+        $deliveryAddress->customer = $customerDelivery;
+
+        $order->shipping_address = $deliveryAddress;
+
+        if($request->hasDifferentAddress == false){
+
+            $order->billing_address = $deliveryAddress;
+
+        }
+
+        else {
+
+            $billingAddress = new address();
+            $billingAddress->address = $request->addressBilling;
+            $billingAddress->zipcode = $request->postCodeBilling;
+            $billingAddress->city = $request->cityBilling;
+
+            $customerBilling = new customer();
+            $customerBilling->firstname = $request->firstNameBilling;
+            $customerBilling->lastname = $request->lastNameBilling;
+            $customerBilling->phone = $request->phoneBilling;
+            $customerBilling->email = $request->emailBilling;
+
+            $billingAddress->customer = $customerBilling;
+
+            $order->billing_address = $billingAddress;
+
+        }
+
+        $order->customer = $customerDelivery;
+/*        $order->status = status*/;
+
+        $order->save();
+        return redirect(route('checkout.index'))->withSuccess(__('form.successfully-stored'));
     }
 
 }
